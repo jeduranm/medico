b701b2a513bdc1807d35bf5545666aede66ac234
Javier Duran <javieredm@gmail.com>
2018-08-15 19:26:04 -0400

    Se separa el index de la plantilla en varias secciones

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.principal')
+  @section('content')
+   
+    @include('layouts.intro')
+    @include('layouts.boxes')
+    @include('layouts.callaction')
+    @include('layouts.service')
+    @include('layouts.doctor')
+    @include('layouts.facilities')
+    @include('layouts.testimonial')
+    @include('layouts.pricing')
+    @include('layouts.partner')
+    @include('layouts.footer')
+   
+  @endsection
