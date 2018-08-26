e33af904c8f8b69898647be8752a122e57ce05da
Javier Duran <javieredm@gmail.com>
2018-08-26 18:45:27 -0400

    Se agrega el controlador al modelo servicio

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Service;
+use App\Http\Requests\ServiceRequest;
+use Storage;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $services= Service::orderBy('id','DESC')->paginate(4);
+        return view('services.index', compact('services'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('services.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ServiceRequest $request)
+    {
+        $service = new Service;
+
+        $service->file          = $request->file;
+        $service->title         = $request->title;
+        $service->description   = $request->description;
+
+        $service->save();
+
+        //SAVE IMAGE
+        if($request->file('file')){
+            $path = Storage::disk('public')->put('image', $request->file('file'));
+            $service->fill(['file' => asset($path)])->save();
+        }
+
+        return redirect()->route('services.index')->with('info', 'El servicio fue guardado');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $service = Service::find($id);
+        return view('services.show', compact('service'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $service = Service::find($id);
+        return view('services.edit', compact('service'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ServiceRequest $request, $id)
+    {
+        $service = Service::find($id);
+
+        //SAVE IMAGE
+        if($request->file('file')){
+            $path = Storage::disk('public')->put('image', $request->file('file'));
+            $service->fill(['file' => asset($path)])->save();
+        }
+
+        $service ->title        =   $request->title;
+        $service ->description  =   $request->description;
+
+        $service->save();
+
+
+        return redirect()->route('services.index')->with('info', 'El servicio fue actualizado');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $service = Service::find($id);
+      $service->delete();
+
+      return back()->with('info','El servicio fue eliminado');
+    }
+}
