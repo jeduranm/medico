4d8102bbd48417e4b8b8eeb0f78588c598f20785
Javier Duran <javieredm@gmail.com>
2018-08-26 23:46:25 -0400

    Se crea el menu doctores en la barra de navegacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,7 @@
                 </a>
                 <a class="navbar-brand" href="{{ route('boxs.index') }}">Boxs</a>
                 <a class="navbar-brand" href="{{ route('services.index') }}">Servicios</a>
+                <a class="navbar-brand" href="{{ route('medicos.index') }}">Doctores</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
