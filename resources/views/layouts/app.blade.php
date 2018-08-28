b05d9e9b1f5d040a1656874572cd61f0fac7ee22
Javier Duran <javieredm@gmail.com>
2018-08-28 16:09:45 -0400

    Se crea el menu Partner en la barra de navegacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,7 @@
                 <a class="navbar-brand" href="{{ route('services.index') }}">Servicios</a>
                 <a class="navbar-brand" href="{{ route('medicos.index') }}">Doctores</a>
                 <a class="navbar-brand" href="{{ route('facilities.index') }}">Instalaciones</a>
+                <a class="navbar-brand" href="{{ route('partners.index') }}">Partners</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
